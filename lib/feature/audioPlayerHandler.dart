b3ce7859ef60fb226716c692730ec2d301fe744a
wrteam-priyansh <wrteam.priyansh@gmail.com>
2021-10-21 18:21:44 +0530

    updated audioPlayerHandler

--- lib/feature/audioPlayerHandler.dart
+++ lib/feature/audioPlayerHandler.dart
@@ -7,7 +7,7 @@ class AudioPlayerHandler extends BaseAudioHandler {
 
   AudioPlayerHandler() {
     _audioPlayer = AudioPlayer();
-    _audioPlayer.playbackEventStream.map(_transformState).pipe(playbackState);
+    _notifyAudioHandlerAboutPlaybackEvents();
   }
 
   AudioPlayer get audioPlayer => _audioPlayer;
@@ -43,31 +43,63 @@ class AudioPlayerHandler extends BaseAudioHandler {
     await _audioPlayer.seek(duration);
   }
 
-  PlaybackState _transformState(PlaybackEvent playbackEvent) {
-    print("Processing state in audio handler : ${_audioPlayer.processingState}");
-    return PlaybackState(
-      processingState: const {
-        ProcessingState.idle: AudioProcessingState.idle,
-        ProcessingState.loading: AudioProcessingState.loading,
-        ProcessingState.buffering: AudioProcessingState.buffering,
-        ProcessingState.ready: AudioProcessingState.ready,
-        ProcessingState.completed: AudioProcessingState.completed,
-      }[_audioPlayer.processingState]!,
-      //to show control like play/pause/next in notification
-      androidCompactActionIndices: const [0, 1, 3],
-      bufferedPosition: _audioPlayer.bufferedPosition,
-      updatePosition: _audioPlayer.position,
+  void _notifyAudioHandlerAboutPlaybackEvents() {
+    _audioPlayer.playbackEventStream.listen((PlaybackEvent event) {
+      playbackState.add(playbackState.value.copyWith(
+        controls: [
+          MediaControl.skipToPrevious,
+          if (_audioPlayer.playing) MediaControl.pause else MediaControl.play,
+          MediaControl.stop,
+          MediaControl.skipToNext,
+        ],
+        systemActions: const {
+          MediaAction.seek,
+        },
+        androidCompactActionIndices: const [0, 1, 3],
+        processingState: const {
+          ProcessingState.idle: AudioProcessingState.idle,
+          ProcessingState.loading: AudioProcessingState.loading,
+          ProcessingState.buffering: AudioProcessingState.buffering,
+          ProcessingState.ready: AudioProcessingState.ready,
+          ProcessingState.completed: AudioProcessingState.completed,
+        }[_audioPlayer.processingState]!,
+        repeatMode: const {
+          LoopMode.off: AudioServiceRepeatMode.none,
+          LoopMode.one: AudioServiceRepeatMode.one,
+          LoopMode.all: AudioServiceRepeatMode.all,
+        }[_audioPlayer.loopMode]!,
+        shuffleMode: (_audioPlayer.shuffleModeEnabled) ? AudioServiceShuffleMode.all : AudioServiceShuffleMode.none,
+        playing: _audioPlayer.playing,
+        updatePosition: _audioPlayer.position,
+        bufferedPosition: _audioPlayer.bufferedPosition,
+        speed: _audioPlayer.speed,
+        queueIndex: event.currentIndex,
+      ));
+      print("Processing state in audio handler : ${_audioPlayer.processingState}");
+      //   PlaybackState(
+      //     processingState: const {
+      //       ProcessingState.idle: AudioProcessingState.idle,
+      //       ProcessingState.loading: AudioProcessingState.loading,
+      //       ProcessingState.buffering: AudioProcessingState.buffering,
+      //       ProcessingState.ready: AudioProcessingState.ready,
+      //       ProcessingState.completed: AudioProcessingState.completed,
+      //     }[_audioPlayer.processingState]!,
+      //     //to show control like play/pause/next in notification
+      //     androidCompactActionIndices: const [0, 1, 3],
+      //     bufferedPosition: _audioPlayer.bufferedPosition,
+      //     updatePosition: _audioPlayer.position,
 
-      controls: [
-        MediaControl.skipToPrevious,
-        _audioPlayer.playing ? MediaControl.pause : MediaControl.play,
-        MediaControl.stop,
-        MediaControl.skipToNext,
-      ],
+      //     controls: [
+      //       MediaControl.skipToPrevious,
+      //       _audioPlayer.playing ? MediaControl.pause : MediaControl.play,
+      //       MediaControl.stop,
+      //       MediaControl.skipToNext,
+      //     ],
 
-      errorMessage: "Error while playing audio",
-      playing: _audioPlayer.playing,
-      speed: _audioPlayer.speed,
-    );
+      //     errorMessage: "Error while playing audio",
+      //     playing: _audioPlayer.playing,
+      //     speed: _audioPlayer.speed,
+      //   );
+    });
   }
 }
